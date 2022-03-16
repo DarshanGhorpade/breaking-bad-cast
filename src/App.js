578ad961b27e7f9175988627adb328c0a1593e96
Darshan Ghorpade <87860114+DarshanGhorpade@users.noreply.github.com>
2022-03-16 10:29:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
-      console.log(result.data);
+      //console.log(result.data);
       // set this result data to the items state
       // so we do that with setItems 
       setItems(result.data);
